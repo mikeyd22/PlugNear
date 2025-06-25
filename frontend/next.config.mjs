52ac720a873624d0326c397574992d3a1c8c3894
mikeyd22 <dayue122@gmail.com>
2025-06-25 12:48:32 -0400

    added nextjs conf

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enable React Strict Mode
+    images: {
+      domains: ['mapbox.com'], // Allow images from Mapbox domain
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'static/fonts/[name].[hash].[ext]',
+          },
+        },
+      });
+      return config;
+    },
+    assetPrefix: '', // Set a custom asset prefix if needed (e.g., for subdirectories)
+  };
+  
+  export default nextConfig;
+  
